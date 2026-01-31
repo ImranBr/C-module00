3398b08142781983e4b43c32dd8f59f10351a974
ImranBr <florian@Flogo18.localdomain>
2026-01-31 16:57:16 +0100

    make 00&01 + class 01 ok

--- ex01/contact.hpp
+++ ex01/contact.hpp
@@ -20,18 +20,24 @@ class Contact
     private:
         std::string _firstName;
         std::string _lastName;
-        std::string _nickName;
+        std::string _nickname;
         std::string _phoneNumber;
         std::string _darkestSecret;
     
     public:
         Contact();
 
-        void    setFirstName(const std::string& firstname);
-        void    setLastName(const std::string& lastname);
-        void    setNickName(const std::string& nickname);
-        void    setPhoneNumber(const std::string& phonenumber);
-        void    setDarkestSecret(const std::string& darkestsecret);
+        void    setFirstName(const std::string& firstName);
+        void    setLastName(const std::string& lastName);
+        void    setNickname(const std::string& nickname);
+        void    setPhoneNumber(const std::string& phoneNumber);
+        void    setDarkestSecret(const std::string& darkestSecret);
 
-        std::string getFirstName
+        std::string getFirstName() const;
+        std::string getLastName() const;
+        std::string getNickname() const;
+        std::string getPhoneNumber() const;
+        std::string getDarkestSecret() const;
 }
+
+#endif
---
+++ ex01/phonebook.hpp
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   phonebook.hpp                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: user <user@student.42.fr>                  #+#  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026-01-31 15:31:00 by user              #+#    #+#             */
+/*   Updated: 2026-01-31 15:31:00 by user             ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PHONEBOOK_HPP
+#define PHONEBOOK_HPP
+
+#include "contact.hpp"
+
+class Phonebook
+{
+    private:
+        Contact _contacts[8];
+        int _count;
+        int _nextIndex;
+    
+    public:
+        PhoneBook();
+
+        void add();
+        void search() const;
+}
+
+#endif
